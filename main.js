70484d7e3558cd0e842ca999c858d5b5bb10c644
Manuel <manueldipilla3@gmail.com>
2020-01-17 16:39:37 +0100

    slider pallini colorati

--- main.js
+++ main.js
@@ -3,11 +3,13 @@ $(document).ready(
     $('.next').click(
       function () {
         next();
+        nextCircle();
       }
     )
     $('.prev').click(
       function () {
         prev();
+        prevCircle();
       }
     )
   }
@@ -37,5 +39,31 @@ function prev() {
   } else {
     active.removeClass('active');
     prevImg.addClass('active');
+  }
+}
+function nextCircle () {
+  var first = $('i.first');
+  var last = $('i.last');
+  var active = $('i.active');
+  var nextCircle = active.next();
+  if (last.hasClass('active') == true) {
+    first.addClass('active');
+    last.removeClass('active');
+  } else {
+    active.removeClass('active');
+    nextCircle.addClass('active');
+  }
+}
+function prevCircle () {
+  var first = $('i.first');
+  var last = $('i.last');
+  var active = $('i.active');
+  var prevCircle = active.prev();
+  if (first.hasClass('active') == true) {
+    last.addClass('active');
+    first.removeClass('active');
+  } else {
+    active.removeClass('active');
+    prevCircle.addClass('active');
   }
 }
